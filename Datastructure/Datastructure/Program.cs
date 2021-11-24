30498e4c672a11ea1a4334a41465d6a185f1e769
shital bramhe <shitalbramhe@gmail.com>
2021-11-24 17:25:00 +0530

    [shital] ADD: unorderd list

---
+++ Datastructure/Datastructure/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Datastructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Unorderedlist unordered = new Unorderedlist();
+            unordered.Operation();
+        }
+    }
+}
---
+++ Datastructure/Datastructure/Unorderedlist.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure
+{
+    internal class Unorderedlist
+    {
+        public void Operation()
+        {
+           // string filepath = @"E:\RFP\DataStructure\Datastructure\Datastructure\Utility\DataFile.txt";
+            string data = File.ReadAllText(@"E:\RFP\DataStructure\Datastructure\Datastructure\Utility\DataFile.txt");
+            Console.WriteLine("-----------------------Data from the file..---------------------------");
+            Console.WriteLine(data);
+           // data = Regex.Replace(data, @"[^a-zA-Z0-9 ]+", "");
+           // Console.WriteLine();
+           // Console.WriteLine(data);
+            string[] words = data.Split(' ');
+           // Console.WriteLine();
+            LinkedList<string> linkedlist = new LinkedList<string>();
+            foreach (string item in words)
+            {
+                linkedlist.AddLast(item);
+            }
+            Console.WriteLine("data into array...");
+            Display(linkedlist);
+            Console.WriteLine("Enter the string you want to search");
+            string input = Console.ReadLine();
+            if (linkedlist.Contains(input))
+            {
+                linkedlist.Remove(input);
+                Console.WriteLine($"{input} removed from the list");
+            }
+            else
+            {
+                linkedlist.AddLast(input);
+                Console.WriteLine($"{input} added to the list");
+            }
+            Console.WriteLine("-----------Data from the file after changes-----------------");
+            Display(linkedlist);
+
+        }
+
+        private void Display(LinkedList<string> linkedlist)
+        {
+            foreach (var item in linkedlist)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+
