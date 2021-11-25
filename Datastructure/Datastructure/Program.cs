d205d7ecd56c397728c3031334979e4542e2ca9b
shital bramhe <shitalbramhe@gmail.com>
2021-11-25 05:46:39 +0530

    [shital] ADD: unordered list

--- Datastructure/Datastructure/Program.cs
+++ Datastructure/Datastructure/Program.cs
@@ -6,8 +6,27 @@ namespace Datastructure
     {
         static void Main(string[] args)
         {
-            Unorderedlist unordered = new Unorderedlist();
-            unordered.Operation();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter Progran number to get excuted \n1. Unordred list \n2. Ordered list \n3. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Unorderedlist unordered = new Unorderedlist();
+                        unordered.Operation();
+                        break;
+                    case 2:
+                        Orderedlist ordered = new Orderedlist();
+                        ordered.Operation();
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+
+                }
+            }
         }
     }
 }
--- Datastructure/Datastructure/Unorderedlist.cs
+++ Datastructure/Datastructure/Unorderedlist.cs
@@ -10,15 +10,10 @@ namespace Datastructure
     {
         public void Operation()
         {
-           // string filepath = @"E:\RFP\DataStructure\Datastructure\Datastructure\Utility\DataFile.txt";
             string data = File.ReadAllText(@"E:\RFP\DataStructure\Datastructure\Datastructure\Utility\DataFile.txt");
             Console.WriteLine("-----------------------Data from the file..---------------------------");
             Console.WriteLine(data);
-           // data = Regex.Replace(data, @"[^a-zA-Z0-9 ]+", "");
-           // Console.WriteLine();
-           // Console.WriteLine(data);
             string[] words = data.Split(' ');
-           // Console.WriteLine();
             LinkedList<string> linkedlist = new LinkedList<string>();
             foreach (string item in words)
             {
@@ -40,9 +35,7 @@ namespace Datastructure
             }
             Console.WriteLine("-----------Data from the file after changes-----------------");
             Display(linkedlist);
-
         }
-
         private void Display(LinkedList<string> linkedlist)
         {
             foreach (var item in linkedlist)
